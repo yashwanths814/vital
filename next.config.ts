f954718879cc41a57d7f727699ec2e37d8f39adc
Inchara S <130211628+IncharaS06@users.noreply.github.com>
2026-02-17 22:28:50 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,7 @@
 // next.config.ts
 import type { NextConfig } from "next";
-import path from "path";
 
-const withNextIntl = require("next-intl/plugin")(
-  path.resolve("./i18n/request.ts")
-);
+const withNextIntl = require("next-intl/plugin")("./i18n/request.ts");
 
 const withPWA = require("next-pwa")({
   dest: "public",
@@ -13,6 +10,8 @@ const withPWA = require("next-pwa")({
   disable: process.env.NODE_ENV === "development",
 });
 
-const nextConfig: NextConfig = { reactStrictMode: true };
+const nextConfig: NextConfig = {
+  reactStrictMode: true,
+};
 
 export default withNextIntl(withPWA(nextConfig));
